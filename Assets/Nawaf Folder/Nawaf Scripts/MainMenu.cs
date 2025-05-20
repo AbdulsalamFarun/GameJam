51e884304a479f0434c5e3d6d703470431d082b7
TheNawaf5 <Jacob5355355@gmail.com>
2025-05-21 00:41:37 +0300

    UI

--- Assets/Nawaf Folder/Nawaf Scripts/MainMenu.cs
+++ Assets/Nawaf Folder/Nawaf Scripts/MainMenu.cs
@@ -3,9 +3,21 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public GameObject settingsPanel;
+
     public void PlayGame()
     {
         SceneManager.LoadScene("Nawaf");
+    }
+
+    public void OpenSettings()
+    {
+        settingsPanel.SetActive(true);
+    }
+
+    public void CloseSettings()
+    {
+        settingsPanel.SetActive(false);
     }
 
     public void QuitGame()
