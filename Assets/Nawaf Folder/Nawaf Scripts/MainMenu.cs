6f0de8a834be0f75096f29685f011406def33d28
MadAlhus <MadAlhus@gmail.com>
2025-05-25 17:48:21 +0300

    Finalll!!!!!!

--- Assets/Nawaf Folder/Nawaf Scripts/MainMenu.cs
+++ Assets/Nawaf Folder/Nawaf Scripts/MainMenu.cs
@@ -7,7 +7,7 @@ public class MainMenu : MonoBehaviour
 
     public void PlayGame()
     {
-        SceneManager.LoadScene("Nawaf");
+        SceneManager.LoadScene("Test Scene");
     }
 
     public void OpenSettings()
